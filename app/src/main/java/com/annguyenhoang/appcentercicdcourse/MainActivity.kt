dee2b3e8ff849c7247e143c1c66a5aa5d2a8436b
Nguyen Hoang An <an.nguyenh@s3corp.com.vn>
2024-07-05 18:06:35 +0700

    Add feature 1

--- app/src/main/java/com/annguyenhoang/appcentercicdcourse/MainActivity.kt
+++ app/src/main/java/com/annguyenhoang/appcentercicdcourse/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.annguyenhoang.appcentercicdcourse
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var changeNameEdt: EditText
+    private lateinit var greetingTv: TextView
+    private lateinit var changeNameBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        changeNameBtn = findViewById(R.id.btn_change_name)
+        greetingTv = findViewById(R.id.tv_helloworld)
+        changeNameEdt = findViewById(R.id.edt_name)
+
+        changeNameBtn.setOnClickListener {
+            greetingTv.text = "Hello ${changeNameBtn.text}"
         }
     }
 }
